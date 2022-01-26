a4f298f7a6ab39d3c21ee91a56a4d390875cd1bc
chad <jcy3589@gmail.com>
2022-01-26 06:17:34 +0000

    init3

---
+++ layouts/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer: React.FC = () => {
+  return <div>푸터입니다</div>;
+};
+
+export default Footer;
---
+++ layouts/Header.tsx
@@ -0,0 +1,5 @@
+const Header: React.FC = () => {
+  return <div>헤더입니다</div>;
+};
+
+export default Header;
