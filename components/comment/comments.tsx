f8217636a63619c1e0c1c40e38bf8961650cda83
JSH-data <super60447@gmail.com>
2022-01-26 15:56:02 +0900

    Feature Comment Component

---
+++ components/comment/comments.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Comments: React.FC = () => {
+  const fakeData = {};
+  return;
+  <></>;
+};
+
+export default Comments;
---
+++ pages/comment.tsx
@@ -0,0 +1,5 @@
+import Comments from "components/commment/comment";
+
+export default function comment() {
+  return <>반갑습니다.</>;
+}
