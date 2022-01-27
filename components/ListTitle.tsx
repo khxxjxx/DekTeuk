7f6f80a298c2e62ccc9a87f1097a86d011e2d420
김희진 <khxxjxx@gmail.com>
2022-01-27 01:43:53 +0000

    Hj/list page

---
+++ components/ListTitle.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const ListTitle = () => {
+  return <MainTitle>자유게시판</MainTitle>;
+};
+
+export default ListTitle;
+
+const MainTitle = styled.div`
+  border-bottom: 1px solid #8946a6;
+`;
---
+++ interface/list.d.ts
@@ -0,0 +1,7 @@
+interface ImageProps {
+  src: string;
+}
+
+interface PageProps {
+  now: boolean;
+}
