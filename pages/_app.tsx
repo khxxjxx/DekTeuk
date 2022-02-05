2df1baaaaa8a1f67245f47620ef53984e8fa18ab
chad <jcy3589@gmail.com>
2022-02-05 07:07:06 +0000

    각 페 infiniteScroll

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,39 +7,39 @@ import {
   dehydrate,
 } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import { useRef } from 'react';
-import { getMyInfo } from '@utils/function';
+// import { useRef } from 'react';
+// import { getMyInfo } from '@utils/function';
 const queryClient = new QueryClient();
-function MyApp({ Component, pageProps, router }: AppProps) {
-  const queryClientRef = useRef<QueryClient>();
-  if (!queryClientRef.current) {
-    queryClientRef.current = new QueryClient({
-      defaultOptions: {
-        queries: { refetchOnWindowFocus: false },
-      },
-    });
-  }
+function MyApp({ Component, pageProps }: AppProps) {
+  // const queryClientRef = useRef<QueryClient>();
+  // if (!queryClientRef.current) {
+  //   queryClientRef.current = new QueryClient({
+  //     defaultOptions: {
+  //       queries: { refetchOnWindowFocus: false },
+  //     },
+  //   });
+  // }
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
+        <Component {...pageProps} />;
         <ReactQueryDevtools initialIsOpen={false} position="top-right" />
       </Hydrate>
     </QueryClientProvider>
   );
 }
-MyApp.getInitialProps = async () => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: { refetchOnWindowFocus: false },
-    },
-  });
-  await queryClient.prefetchQuery('user', getMyInfo);
+// MyApp.getInitialProps = async () => {
+//   const queryClient = new QueryClient({
+//     defaultOptions: {
+//       queries: { refetchOnWindowFocus: false },
+//     },
+//   });
+//   await queryClient.prefetchQuery('user', getMyInfo);
 
-  return {
-    pageProps: {
-      dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-    },
-  };
-};
+//   return {
+//     pageProps: {
+//       dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+//     },
+//   };
+// };
 export default MyApp;
