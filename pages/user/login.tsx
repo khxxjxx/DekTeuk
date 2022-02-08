b46a2ab1cacbbf6c5c5907214b8eb1d3f960db46
홍순범 <sounbeom@gmail.com>
2022-02-08 11:58:01 +0900

    login-signup

---
+++ pages/user/login.tsx
@@ -0,0 +1,172 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+import styled from '@emotion/styled';
+import TextField from '@mui/material/TextField';
+import {
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+
+import { db, auth } from '@firebase/firebase';
+import { useRouter } from 'next/router';
+
+export default function Login() {
+  const router = useRouter();
+  const provider = new GoogleAuthProvider();
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const onChangeInput = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+  ) => {
+    const { name, value } = e.target;
+    if (name === 'email') setEmail(value);
+    else if (name === 'password') setPassword(value);
+  };
+
+  const loginWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then(async (result) => {
+        // The signed-in user info.
+        const user = result.user;
+        console.log(user.uid);
+        const docSnap = await getDoc(doc(db, 'user', user.uid));
+        if (docSnap.exists()) {
+          console.log('이미 가입되어 있습니다.');
+          router.push('/');
+        } else {
+          console.log('가입되어있지 않습니다.');
+          router.push('/user/google');
+        }
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
+  const SignUpSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        router.push('/');
+      })
+      .catch((error) => console.log(error.code, error.message));
+  };
+  return (
+    <>
+      <Main>
+        <h1 style={{ color: '#8946A6' }}>로그인</h1>
+        <form onSubmit={SignUpSubmitHandler}>
+          <WrapContents>
+            <WrapInput>
+              <Label>Email</Label>
+              <TextField
+                required
+                fullWidth
+                variant="outlined"
+                margin="dense"
+                name="email"
+                value={email}
+                onChange={onChangeInput}
+              />
+            </WrapInput>
+            <WrapInput>
+              <Label>비밀번호</Label>
+              <TextField
+                required
+                fullWidth
+                type="password"
+                variant="outlined"
+                margin="dense"
+                name="password"
+                value={password}
+                onChange={onChangeInput}
+              />
+            </WrapInput>
+            <WrapButton>
+              <Button>
+                <Link href="/user/signup">회원가입</Link>
+              </Button>
+              <Button type="button" onClick={loginWithGoogle}>
+                Google
+              </Button>
+            </WrapButton>
+            <SubmitButton type="submit">로그인</SubmitButton>
+          </WrapContents>
+        </form>
+      </Main>
+    </>
+  );
+}
+
+const Main = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const WrapContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const WrapInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+  width: 100%;
+`;
+
+const Button = styled.button`
+  background: #8946a6;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  width: 80px;
+  height: 24px;
+  font-size: 12px;
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const WrapButton = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px;
+  width: 313px;
+`;
+
+const SubmitButton = styled.button`
+  background: #8946a6;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  width: 173px;
+  height: 58px;
+  font-size: 20px;
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const Label = styled.label`
+  color: #8946a6;
+  margin: 5px;
+`;
---
+++ pages/user/signup.tsx
@@ -0,0 +1,232 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import InputAdornment from '@mui/material/InputAdornment';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { db, auth } from '@firebase/firebase';
+import {
+  doc,
+  setDoc,
+  getDocs,
+  collection,
+  query,
+  where,
+} from 'firebase/firestore';
+
+import { useRouter } from 'next/router';
+
+export default function Signup() {
+  const router = useRouter();
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [checkPassword, setCheckPassword] = useState<string>('');
+  const [nickname, setNickname] = useState<string>('');
+  const [error, setError] = useState<string>('');
+
+  const userInitData = {
+    nickname: nickname,
+    jobSector: '',
+    validRounges: [
+      {
+        title: '',
+        url: '',
+      },
+    ],
+    myChatting: [
+      {
+        roomName: '',
+        roomId: '',
+        isGroup: false,
+        lastMessage: {
+          content: '',
+          updatedAt: '',
+        },
+        unreadCount: 0,
+      },
+    ],
+    notification: true,
+  };
+
+  const onInputChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>,
+  ) => {
+    const { name, value } = e.target;
+    if (name === 'email') setEmail(value);
+    else if (name === 'password') setPassword(value);
+    else if (name === 'checkPassword') setCheckPassword(value);
+    else if (name === 'nickname') setNickname(value);
+  };
+
+  const SignUpSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (checkPassword !== password) {
+      alert('비밀번호가 다릅니다!');
+    } else {
+      await createUserWithEmailAndPassword(auth, email, password)
+        .then(async (userCredential) => {
+          await setDoc(doc(db, 'user', userCredential.user.uid), userInitData);
+          router.push('/');
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  const checkNickname = async () => {
+    const nicknameCheckQuery = query(
+      collection(db, 'user'),
+      where('nickname', '==', nickname),
+    );
+    const nicknameCheckSnap = await getDocs(nicknameCheckQuery);
+    if (nicknameCheckSnap.docs.length !== 0) {
+      alert('닉네임이 중복되었습니다!');
+    }
+  };
+
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // ! nullable - null, undefined 허용
+    // ? optional - 필수값이 아닌 옵셔널한 값, null, undefined 일 경우 동작을 멈추고 undefined 리턴
+    const files = e.target.files!;
+    console.log(files[0]);
+  };
+
+  return (
+    <>
+      <Main>
+        <h1 style={{ color: '#8946A6' }}>회원가입</h1>
+        <form onSubmit={SignUpSubmitHandler}>
+          <WrapContents>
+            <WrapInput>
+              <Label>Email</Label>
+              <TextField
+                required
+                variant="outlined"
+                placeholder="Email 주소를 입력해 주세요."
+                name="email"
+                value={email}
+                onChange={onInputChange}
+              />
+            </WrapInput>
+            <WrapInput>
+              <Label>비밀번호</Label>
+              <TextField
+                required
+                fullWidth
+                type="password"
+                placeholder="비밀번호는 6자리 이상 입력해주세요."
+                variant="outlined"
+                margin="dense"
+                name="password"
+                value={password}
+                onChange={onInputChange}
+              />
+              <TextField
+                required
+                type="password"
+                placeholder="비밀번호를 한 번더 입력해 주세요."
+                variant="outlined"
+                margin="dense"
+                name="checkPassword"
+                value={checkPassword}
+                onChange={onInputChange}
+              />
+            </WrapInput>
+            <WrapInput>
+              <Label>닉네임</Label>
+              <TextField
+                required
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <CheckButton type="button" onClick={checkNickname}>
+                        중복확인
+                      </CheckButton>
+                    </InputAdornment>
+                  ),
+                }}
+                variant="outlined"
+                margin="dense"
+                name="nickname"
+                placeholder="닉네임을 입력해 주세요."
+                value={nickname}
+                onChange={onInputChange}
+              />
+            </WrapInput>
+            <label htmlFor="contained-button-file">
+              <Input
+                accept="image/*"
+                id="contained-button-file"
+                type="file"
+                onChange={onFileChange}
+              />
+              <Button variant="contained" component="span">
+                Upload
+              </Button>
+            </label>
+            <SubmitButton type="submit">회원가입</SubmitButton>
+          </WrapContents>
+        </form>
+      </Main>
+    </>
+  );
+}
+
+const Main = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const WrapContents = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const WrapInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+  width: 100%;
+`;
+
+const CheckButton = styled.button`
+  background: #8946a6;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  width: 60px;
+  height: 24px;
+  margin: 5px;
+  font-size: 12px;
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const SubmitButton = styled.button`
+  background: #8946a6;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  width: 173px;
+  height: 58px;
+  font-size: 20px;
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const Label = styled.label`
+  color: #8946a6;
+  margin: 5px;
+  ::after {
+    content: '*';
+    color: red;
+  }
+`;
+const Input = styled('input')({
+  display: 'none',
+});
