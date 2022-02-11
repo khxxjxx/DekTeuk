d4d88bbcb06ba72c77993861853aba8ef6468ecd
chad <jcy3589@gmail.com>
2022-02-11 07:50:18 +0000

    firebase dotenv 설정

--- firebase/firebaseAdmin.tsx
+++ firebase/firebaseAdmin.tsx
@@ -9,11 +9,11 @@ import serviceAccount from './secret.json';
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: serviceAccount.private_key,
-      clientEmail: serviceAccount.client_email,
-      projectId: serviceAccount.project_id,
+      privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+      clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
+      projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     }),
-    databaseURL: 'https://devily-test-default-rtdb.firebaseio.com',
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
   });
 }
 
