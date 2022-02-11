39a528727233320688eb36460ddedf3d19a04dca
chad <jcy3589@gmail.com>
2022-02-11 09:22:22 +0000

    firebase admin에 privateKey 끝에 replace(/\\n/g, n)추가 (bugFix)

--- firebase/firebaseAdmin.tsx
+++ firebase/firebaseAdmin.tsx
@@ -9,7 +9,7 @@ import serviceAccount from './secret.json';
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY,
+      privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
       projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     }),
