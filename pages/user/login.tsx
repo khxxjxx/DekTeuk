29ae4a50f91b08140572bb57f3b6e4a2e9b47488
홍순범 <sounbeom@gmail.com>
2022-02-18 15:51:47 +0900

    다크모드 적용

--- pages/user/login.tsx
+++ pages/user/login.tsx
@@ -149,6 +149,10 @@ export const getServerSideProps: GetServerSideProps = async (
 
 const Title = styled.h1`
   color: ${({ theme }: any) => theme.mainColorViolet};
+
+  @media (prefers-color-scheme: dark) {
+    color: ${({ theme }: any) => theme.mainColorBlue};
+  }
 `;
 
 const Main = styled.div`
@@ -206,6 +210,9 @@ const Button = styled.button`
   :hover {
     opacity: 0.8;
   }
+  @media (prefers-color-scheme: dark) {
+    background: ${({ theme }: any) => theme.mainColorBlue};
+  }
 `;
 
 const WrapButton = styled.div`
@@ -234,9 +241,15 @@ const SubmitButton = styled.button`
   :hover {
     opacity: 0.8;
   }
+  @media (prefers-color-scheme: dark) {
+    background: ${({ theme }: any) => theme.mainColorBlue};
+  }
 `;
 
 const Label = styled.label`
   color: ${({ theme }: any) => theme.mainColorViolet};
   margin: 5px;
+  @media (prefers-color-scheme: dark) {
+    color: ${({ theme }: any) => theme.mainColorBlue};
+  }
 `;
